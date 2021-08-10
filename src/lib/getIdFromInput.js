1766f5370c61155cfb696bf3bb428fe58db6e2e1
Michal Koptewicz <mkoptewicz@gmail.com>
2021-08-10 10:27:19 +0200

    create helper functions for getting ids

---
+++ src/lib/getIdFromInput.js
@@ -0,0 +1,23 @@
+const getYouTubeId = input => {
+  //assuming YouTube video's id is always 11 characters long
+  if (
+    input.startsWith("https://www.youtube.com/") ||
+    input.startsWith("https://youtu.be")
+  ) {
+    return input.slice(-11);
+  }
+  return input;
+};
+const getVimeoId = input => {
+  if (input.includes("vimeo.com")) {
+    const idStart = input.lastIndexOf("/") + 1;
+    return input.slice(idStart);
+  }
+  return input;
+};
+
+const getIdFromInput = {
+  youtube: getYouTubeId,
+  vimeo: getVimeoId,
+};
+export default getIdFromInput;

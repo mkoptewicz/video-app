21cc9305781a51ba2a7b3c67aa544f457e60ae17
Michal Koptewicz <mkoptewicz@gmail.com>
2021-08-10 12:17:40 +0200

    add only favourites filter

--- src/components/VideosList.js
+++ src/components/VideosList.js
@@ -1,12 +1,38 @@
 import VideoDetails from "./VideoDetails";
-import { Container, Row } from "react-bootstrap";
+import { Container, Row, Button } from "react-bootstrap";
+import { useState } from "react";
 
 const VideosList = ({ videos, onDelete, onFavourite }) => {
+  const [filterIsActive, setFilterIsActive] = useState(false);
+
+  const filteredVideos = filterIsActive
+    ? videos.filter(vid => vid.isFavourite)
+    : videos;
+
+  const filterButton = filterIsActive ? (
+    <Button
+      onClick={() => setFilterIsActive(false)}
+      variant="warning"
+      className="mb-3"
+    >
+      Show all
+    </Button>
+  ) : (
+    <Button
+      onClick={() => setFilterIsActive(true)}
+      variant="warning"
+      className="mb-3"
+    >
+      Show favourites
+    </Button>
+  );
+
   return (
     <section>
       <Container>
+        {filterButton}
         <Row className="justify-content-center justify-content-md-start">
-          {videos.map(video => (
+          {filteredVideos.map(video => (
             <VideoDetails
               key={video.id}
               video={video}

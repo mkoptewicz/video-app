8a19b1c220d0f64254ebd5f82e8e3a3858394762
Michal Koptewicz <mkoptewicz@gmail.com>
2021-08-12 10:48:36 +0200

    add correct display for over billion views

--- src/lib/formatNumber.js
+++ src/lib/formatNumber.js
@@ -2,6 +2,9 @@ const formatNumber = num => {
   if (num < 1e3) {
     return num;
   }
+  if (num >= 1e9) {
+    return +(num / 1e9).toFixed(1) + "B";
+  }
   if (num >= 1e6) {
     return +(num / 1e6).toFixed(1) + "M";
   }

7ae17288b04cf8ab58332cf92ecdd8b004db09c2
Michal Koptewicz <mkoptewicz@gmail.com>
2021-08-13 12:20:37 +0200

    remove commented out code

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,6 @@
 const Header = () => {
   return (
     <header>
-      {/* <div>
-        <div className="logo"></div>
-        <button>Log out</button>
-      </div> */}
       <h1 className="m-4 display-3 text-center">
         Keep all your favourite videos in one place!
       </h1>
